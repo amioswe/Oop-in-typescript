615fe5cdced57a3cfd6a76dd8981ca86d8852468
amioswe <amioswe@gmail.com>
2026-09-18 09:57:29 +0600

    obejct create ts

---
+++ dist/object.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const StudentInfo = (name, phone, email) => {
+    const obj = { name, phone, email };
+};
+const showThedetailse = StudentInfo('amiodas', 7434793, '834834804');
+console.log(showThedetailse);
+//# sourceMappingURL=object.js.map
---
+++ src/object.ts
@@ -0,0 +1,14 @@
+const StudentInfo=(name:string,phone:number,email:string)=>{
+
+
+const obj={name,phone,email};
+
+return obj;
+
+
+}
+
+const showThedetailse=StudentInfo('amiodas',7434793,'834834804');
+
+
+console.log(showThedetailse);
